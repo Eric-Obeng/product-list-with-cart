ef8430ef1cfb06953dba0c245592f2c9920938f7
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-22 14:51:23 +0000

    cart component created

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../interface/product.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,4 +12,26 @@ import { CommonModule } from '@angular/common';
 })
 export class CartComponent {
   emptyCart: number = 0;
+  cartItems: Product[] = [];
+
+  private cartService = inject(CartService);
+
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe((data) => {
+      this.cartItems = data;
+      this.emptyCart = this.cartItems.length;
+    });
+  }
+
+  getTotalItems(): number {
+    return this.cartService.getTotalItems();
+  }
+
+  removeFromCart(product: Product): void {
+    this.cartService.removeFromCart(product);
+  }
+
+  getTotalPrice() {
+    return this.cartService.getTotalPrice();
+  }
 }
